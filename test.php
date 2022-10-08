8aa52668b50aa0c0751958a5fc4602faa874d76b
e3ddd <60689821+e3ddd@users.noreply.github.com>
2022-10-08 14:26:17 +0300

    Add files via upload

--- test.php
+++ test.php
@@ -43,6 +43,34 @@ for ($i=0;$i<$item->length;$i++){
 
 
 
+$xml = new DOMDocument();
+$xml->formatOutput = true;
+$main = $xml->createElement('products');
+$body = "";
+$productXML = '<?xml version="1.0" encoding="UTF-8" ?>';
+$productXML .= "<". $main->tagName . ">";
+foreach ($products as $product){
+    $item = $xml->createElement('product');
+    $name = $xml->createElement('name', $product['title']);
+    $price = $xml->createElement('price', $product['price']);
+    $item->appendChild($name);
+    $item->appendChild($price);
+    $productXML .= "<". $item->tagName . ">"
+        . "<". $name->tagName . ">" . $name->textContent . "</". $name->tagName . ">"
+        . "<". $price->tagName . ">" . $price->textContent . "</". $price->tagName . ">"
+        . "</". $item->tagName . ">";
+}
+
+$productXML .= "</". $main->tagName . ">";
+
+
+$xml->loadXML($productXML);
+$path = 'test.xml';
+echo $xml->save($path);
+
+$xmlFile = new SimpleXMLElement($productXML);
+
+echo "<pre>"; print_r($xmlFile);
 
 
 
@@ -50,4 +78,10 @@ for ($i=0;$i<$item->length;$i++){
 
 
 
-
+//
+//echo "<pre>"; print_r("&lt". $main->localName . "&gt");
+//echo "<pre>"; echo "   "; print_r("&lt". $item->localName . "&gt");
+//echo "<pre>"; echo "    "; echo "&lt". "name" . "&gt"; print_r($name->textContent);echo "&lt/". "name" . "&gt";
+//echo "<pre>"; echo "    "; echo "&lt". "price" . "&gt"; print_r($price->textContent);echo "&lt/". "price" . "&gt";
+//echo "<pre>"; echo "   "; print_r("&lt/". $item->localName . "&gt");
+//echo "<pre>"; print_r("&lt/". $main->localName . "&gt");
